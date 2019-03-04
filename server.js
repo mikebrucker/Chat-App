6b52a4182e1fd8af72e3e8c63df3f7e640ce6125
Mike Brucker <disposition11@gmail.com>
2019-03-04 14:55:36 -0500

    register and login

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const path = require("path");
 
 const users = require("./routes/api/users");
 const messages = require("./routes/api/messages");
 const chatroom = require("./routes/api/chatroom");
 
 const app = express();
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Database Config
 const db = require("./config/keys").mongoURI;
@@ -16,12 +23,26 @@ mongoose
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World"));
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require("./config/passport")(passport);
 
 // Use Routes
 app.use("/api/users", users);
 app.use("/api/messages", messages);
 app.use("/api/chatroom", chatroom);
+
+// Server static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendfile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
